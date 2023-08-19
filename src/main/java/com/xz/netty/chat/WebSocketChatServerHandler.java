ec09cb063b33ac697c617343b3820a2ce6dba79a
xz <1187567532@qq.com>
2023-08-19 21:06:00 +0800

    feat: multi client support.

--- src/main/java/com/xz/netty/chat/WebSocketChatServerHandler.java
+++ src/main/java/com/xz/netty/chat/WebSocketChatServerHandler.java
@@ -1,5 +1,6 @@
 package com.xz.netty.chat;
 
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.group.ChannelGroup;
@@ -12,7 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.Locale;
 
-/**
+/**a
  * @author xz
  * @date 2023/7/31 17:44
  */
@@ -23,21 +24,43 @@ public class WebSocketChatServerHandler extends SimpleChannelInboundHandler<WebS
     protected void channelRead0(ChannelHandlerContext ctx, WebSocketFrame frame) throws Exception {
         if (frame instanceof TextWebSocketFrame) {
             // Send the uppercase string back.
-            String request = ((TextWebSocketFrame) frame).text();
-            ctx.channel().writeAndFlush(new TextWebSocketFrame(request.toUpperCase(Locale.US)));
+            String request = ctx.channel().remoteAddress() + ": " + ((TextWebSocketFrame) frame).text();
+            chatGroup.writeAndFlush(new TextWebSocketFrame(request.toUpperCase(Locale.US)));
         } else {
             log.error("unsupported frame type: " + frame.getClass().getName());
         }
     }
 
-//    @Override
-//    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
-//        if (evt instanceof WebSocketServerProtocolHandler.HandshakeComplete) {
-//            //Channel upgrade to websocket, remove WebSocketIndexPageHandler.
-//            ctx.pipeline().remove(WebSocketIndexPageHandler.class);
-//        } else {
-//            super.userEventTriggered(ctx, evt);
-//        }
-//    }
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+    }
 
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        String request = ctx.channel().remoteAddress() + ": 已离开群聊";
+        chatGroup.writeAndFlush(new TextWebSocketFrame(request.toUpperCase(Locale.US)));
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        Channel channel = ctx.channel();
+        System.out.println("client added :" + channel.remoteAddress());
+
+        String request = ctx.channel().remoteAddress() + ": 已加入聊天";
+        chatGroup.writeAndFlush(new TextWebSocketFrame(request.toUpperCase(Locale.US)));
+
+        chatGroup.add(ctx.channel());
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        // 上下线对客户端状态作标识.
+        log.info(ctx.channel().remoteAddress() + " 已上线");
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        log.info(ctx.channel().remoteAddress() + " 已下线");
+    }
 }
